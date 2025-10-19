34c295e36cc4d23ded761f8c3c3845e75f4a4e4b
Tom Okafor <tmeka008@gmail.com>
2025-10-19 22:35:09 +0100

    feat: import and embed Button component, format code structure and update tailwind style classes

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,12 +1,15 @@
+'use client'
 import Image from "next/image";
 import hult_prize_nigeria_logo from "../../../public/HP_NigeriaHP.png";
 import Link from "next/link";
 import { NAVLINKS } from "@/src/lib/constants";
 import Navlink from "../ui/Navlink";
+import Button from "../ui/Button";
+import { redirect } from "next/navigation";
 
 export default function Header() {
   return (
-    <header className="flex px-8 py-4 justify-between">
+    <header className="flex px-8 py-4 justify-between items-center">
       <Link href={"/"}>
         <Image
           src={hult_prize_nigeria_logo}
@@ -14,10 +17,22 @@ export default function Header() {
           className="w-[60px] aspect-1.154/1 object-cover"
         />
       </Link>
-      <nav className="flex gap-4 items-center">
-        {NAVLINKS.map((each_link, index) => (
-          <Navlink key={index} href={each_link.href} title={each_link.title} />
-        ))}
+      <nav className="flex gap-8 items-center">
+        <div className="flex items-center gap-6">
+          {NAVLINKS.map((each_link, index) => (
+            <Navlink
+              key={index}
+              href={each_link.href}
+              title={each_link.title}
+            />
+          ))}
+        </div>
+        <Button
+          text="Register to compete"
+          onclick={() => {
+            redirect("/register");
+          }}
+        />
       </nav>
     </header>
   );
