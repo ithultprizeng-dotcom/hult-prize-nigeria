5180602bd4f2badcb7481c4500dea2414a2ea03c
Tom Okafor <tmeka008@gmail.com>
2025-10-19 18:43:47 +0100

    feat: add interface and Navlinks constant for the website navigation links

---
+++ src/lib/constants.ts
@@ -0,0 +1,14 @@
+import { title } from "process";
+
+interface NavlinksType {
+  href: string;
+  title: string;
+}
+
+const NAVLINKS: NavlinksType[] = [
+  { href: "competition", title: "the competition" },
+  { href: "how-it-works", title: "how it works" },
+  { href: "get-involved", title: "get involved" },
+  { href: "hult-prize-winners", title: "winners + finalists" },
+  { href: "about-hult-prize", title: "about us" },
+];
