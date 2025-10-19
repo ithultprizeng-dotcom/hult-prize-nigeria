60f738dd3d582d7cb7160beabd8f26d1136b4e19
Tom Okafor <tmeka008@gmail.com>
2025-10-19 19:01:03 +0100

    feat: add Header component to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
+import Header from "@/src/components/layout/Header";
+
 export default function Home() {
   return (
     <>
-      <h1 className="font-black text-8xl text-green-400 ">
-        HULT PRIZE NIGERIA
-      </h1>
+      <Header />
     </>
   );
 }
