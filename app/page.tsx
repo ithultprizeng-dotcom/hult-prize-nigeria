1cc5188c8e7529593e0eb272cc738c895dc4d4ff
Tom Okafor <tmeka008@gmail.com>
2025-10-20 07:09:14 +0100

    feat: import and render Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/src/components/layout/Header";
+import Hero from "@/src/components/layout/Hero";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Hero />
     </>
   );
 }
