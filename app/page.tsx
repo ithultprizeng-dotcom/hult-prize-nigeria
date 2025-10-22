3e75499a080081fcae897c3e52e4771c54187665
Tom Okafor <tmeka008@gmail.com>
2025-10-22 01:58:14 +0100

    feat: import and render intro section component and create section dividers for aesthetic use

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,47 @@
 import Header from "@/src/components/layout/Header";
 import Hero from "@/src/components/layout/Hero";
+import Intro from "@/src/components/layout/Intro";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <SectionDivisionOne />
+      <Intro />
+      <section className="h-50 flex items-end bg-pink overflow-hidden">
+        <div className="bg-white h-50 w-50 rounded-tr-full flex justify-end items-start flex-col gap-1 pl-2">
+          <div className="flex gap-1 justify-center items-center">
+            <div className="size-10 bg-aloe rounded-full"></div>
+            <div className="size-10 bg-pink rounded-tr-full"></div>
+            <div className="size-10 bg-aloe rounded-full"></div>
+          </div>
+          <div className="flex gap-1 justify-center items-center">
+            <div className="size-10 bg-pink rounded-tr-full"></div>
+            <div className="size-10 bg-aloe rounded-full"></div>
+            <div className="size-10 bg-pink rounded-tl-full"></div>
+          </div>
+        </div>
+        <div className="size-40 rotate-45 translate-y-[50%] translate-x-[-30px] bg-white ml-auto flex justify-center items-center">
+          <div className="rounded-full size-15 bg-pink -translate-5"></div>
+        </div>
+      </section>
     </>
   );
 }
+
+const SectionDivisionOne = () => {
+  return (
+    <section className="h-40 flex items-end">
+      <div className="flex items-end gap-3">
+        <div className="h-30 w-30 bg-pink rounded-tr-[200%]"></div>
+        <div className="h-25 w-25 bg-pink rotate-45 translate-y-[50%]"></div>
+      </div>
+      <div className="ml-auto flex gap-0.5">
+        {["", "", ""].map((item, index) => (
+          <div key={index} className="h-20 bg-pink w-40 rounded-t-full"></div>
+        ))}
+      </div>
+    </section>
+  );
+};
