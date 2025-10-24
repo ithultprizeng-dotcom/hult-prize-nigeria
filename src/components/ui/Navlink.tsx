5593767cb4827ffafaad45f6ac5fc6f07a4dfbe6
Tom Okafor <tmeka008@gmail.com>
2025-10-24 06:38:05 +0100

    refactor: change font family of nav link items

--- src/components/ui/Navlink.tsx
+++ src/components/ui/Navlink.tsx
@@ -5,7 +5,7 @@ export default function Navlink({ href, title }: NavlinksType) {
   return (
     <Link
       href={href}
-      className="text-caption font-figtree text-midnight font-semibold capitalize hover:tracking-wide duration-200 hover:text-pink"
+      className="text-caption font-body text-midnight font-semibold capitalize hover:tracking-wide duration-200 hover:text-pink"
     >
       {title}
     </Link>
