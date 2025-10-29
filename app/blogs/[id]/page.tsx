2ff6f6b63d0256e373e1fefd3b9593885865c605
Tom Okafor <tmeka008@gmail.com>
2025-10-29 05:15:13 +0100

    feat: create blog page with dummy txt

---
+++ app/blogs/[id]/page.tsx
@@ -0,0 +1,10 @@
+export default function Blog() {
+  return (
+    <main>
+      <h1 className="text-hero text-midnight font-figtree font-black">
+        this is the individual blog page where the blog content will be
+        displayed
+      </h1>
+    </main>
+  );
+}
