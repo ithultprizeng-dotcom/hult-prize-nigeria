9950dbf29b67ff8b7f17a1bec99aab2b30a3b7d3
Tom Okafor <tmeka008@gmail.com>
2025-10-31 06:56:21 +0100

    feat: update styles and use props in place of dummy text

--- src/components/ui/BlogCard.tsx
+++ src/components/ui/BlogCard.tsx
@@ -1,13 +1,14 @@
+"use client";
 import { BlogCardType } from "@/src/lib/constants";
 import Image from "next/image";
 import Button from "./Button";
-export default function BlogCard() {
+export default function BlogCard({ title, imageSrc, author }: BlogCardType) {
   return (
-    <article className="flex flex-col gap-10 w-[300px]">
-      <div className="grid grid-cols-1 w-[300px] h-[300px]">
+    <article className="flex flex-col gap-10 w-[350px]">
+      <div className="grid grid-cols-1 w-[350px] h-[350px]">
         <div className="article-image bg-aloe col-start-1 col-end-2 row-start-1 row-end-2 z-2 relative">
           <Image
-            src="/images/image6.jpg"
+            src={imageSrc}
             alt="group photograh"
             fill
             className="object-cover"
@@ -17,14 +18,14 @@ export default function BlogCard() {
       </div>
       <div className="flex flex-col gap-2">
         <h3 className="font-bold text-black font-figtree text-body-lg capitalize leading-[1.3]">
-          This is a blog post header
+          {title}
         </h3>
-        <p className="font-body text-[#808080] text-body leading-[1.5]">
-          Ad amet dolor esse magna sunt velit eiusmod adipisicing exercitation
-          sunt.
+        <p className="font-body text-[#707070] text-body font-semibold">
+          {author}
         </p>
+
         <Button
-          text="read more"
+          text="read blog"
           classnames="reversed button hov-pink bg-pink grow-0 w-fit pr-8 hover:bg-black"
         />
       </div>
