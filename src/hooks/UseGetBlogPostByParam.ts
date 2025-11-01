bdaa0c5acbfea5079b648f5687ddfcf850b847d6
Tom Okafor <tmeka008@gmail.com>
2025-11-01 18:13:28 +0100

    feat: update use get blog posts hook to fetch data from the api

--- src/hooks/UseGetBlogPostByParam.ts
+++ src/hooks/UseGetBlogPostByParam.ts
@@ -1,8 +1,19 @@
 "use client";
 import { useParams } from "next/navigation";
-import { BlogPosts } from "../lib/constants";
+import { useEffect, useState } from "react";
+import { fetchSingleBlogPost } from "../lib/services";
+import { BlogAPIResponseData } from "../lib/types";
 
 export default function UseGetBlogPostByParam() {
   const { id } = useParams();
-    return BlogPosts.find((eachPost) => eachPost.id === id);
+  const [blogPost, setBlogPost] = useState<BlogAPIResponseData | null>(null);
+  useEffect(() => {
+    async function getSingleBlogPost() {
+      if (!id) return;
+      const singleBlogPost = await fetchSingleBlogPost(id?.toString());
+      setBlogPost(singleBlogPost);
+    }
+    getSingleBlogPost();
+  }, []);
+  return blogPost;
 }
