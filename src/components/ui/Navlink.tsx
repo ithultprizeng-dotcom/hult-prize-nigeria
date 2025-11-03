247df5e501d7c32d24b0a9ded0f9da3ca70cce55
Tom Okafor <tmeka008@gmail.com>
2025-11-03 14:50:50 +0100

    style: add border to navlink

--- src/components/ui/Navlink.tsx
+++ src/components/ui/Navlink.tsx
@@ -5,7 +5,7 @@ export default function Navlink({ href, title }: NavlinksType) {
   return (
     <Link
       href={href}
-      className="text-caption font-body text-midnight font-semibold capitalize hover:tracking-wide duration-200 hover:text-pink"
+      className="text-caption font-body text-midnight font-semibold capitalize hover:tracking-wide duration-200 hover:text-pink border-2 rounded-full px-3 py-2"
     >
       {title}
     </Link>
