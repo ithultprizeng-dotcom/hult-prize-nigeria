64ba062c92ff9bc9bacbf398bd51c2682cc9d80e
Tom Okafor <tmeka008@gmail.com>
2025-11-06 17:49:47 +0100

    style: adjust the widths andn heights for the second section division for a more responsive look and visual balance across devices

--- app/page.tsx
+++ app/page.tsx
@@ -48,24 +48,24 @@ const SectionDivisionOne = () => {
 
 const SectionDivisionTwo = () => {
   return (
-    <section className="h-50 flex items-end bg-aloe overflow-hidden">
-      <div className="bg-white h-50 w-50 rounded-tr-full flex justify-end items-start flex-col gap-1 pl-2">
+    <section className="h-25 sm:h-40 md:h-50 flex items-end bg-aloe overflow-hidden">
+      <div className="bg-white sm:size-40 size-25 md:size-50 rounded-tr-full flex justify-end items-start flex-col gap-1 pl-2">
         <div className="flex gap-1 justify-center items-center">
-          <div className="size-10 bg-aloe rounded-full"></div>
-          <div className="size-10 bg-pink rounded-tr-full"></div>
-          <div className="size-10 bg-aloe rounded-full"></div>
+          <div className="size-5 sm:size-8 md:size-10 bg-aloe rounded-full"></div>
+          <div className="size-5 sm:size-8 md:size-10 bg-pink rounded-tr-full"></div>
+          <div className="size-5 sm:size-8 md:size-10 bg-aloe rounded-full"></div>
         </div>
         <div className="flex gap-1 justify-center items-center">
-          <div className="size-10 bg-pink rounded-tr-full"></div>
-          <div className="size-10 bg-aloe rounded-full"></div>
-          <div className="size-10 bg-pink rounded-tl-full"></div>
+          <div className="size-5 sm:size-8 md:size-10 bg-pink rounded-tr-full"></div>
+          <div className="size-5 sm:size-8 md:size-10 bg-aloe rounded-full"></div>
+          <div className="size-5 sm:size-8 md:size-10 bg-pink rounded-tl-full"></div>
         </div>
       </div>
-      <div className="size-40 rotate-45 translate-y-[50%] translate-x-[-30px] bg-white ml-auto flex justify-center items-center">
-        <div className="rounded-full size-15 bg-pink -translate-6 grid place-items-center">
-          <div className="rounded-md size-7 bg-white grid place-items-center">
-            <div className="rounded size-4 bg-aloe grid place-items-center">
-              <div className="rounded size-2 bg-white"></div>
+      <div className="sm:size-40 size-30 rotate-45 translate-y-[50%] translate-x-[-30px] bg-white ml-auto flex justify-center items-center">
+        <div className="rounded-full size-10 sm:size-15 bg-pink -translate-6 grid place-items-center">
+          <div className="rounded-md size-6 sm:size-7 bg-white grid place-items-center">
+            <div className="rounded size-3 sm:size-4 bg-aloe grid place-items-center">
+              <div className="rounded size-1 sm:size-2 bg-white"></div>
             </div>
           </div>
         </div>
