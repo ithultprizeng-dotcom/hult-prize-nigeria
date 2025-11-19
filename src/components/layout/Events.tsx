4bc418d4f53b22b049144c9f69d9b0a0a4065704
Tom Okafor <tmeka008@gmail.com>
2025-11-19 13:41:43 +0100

    refactor: extract reusable component into Event Cards Block component

--- src/components/layout/Events.tsx
+++ src/components/layout/Events.tsx
@@ -1,6 +1,5 @@
-
-import { events } from "@/src/lib/constants";
-import EventCard from "../ui/EventCard";
+"use client";
+import { EventCardsBlock } from "../ui/EventCardsBlock";
 
 const paragraphStyleClasses =
   "font-body font-normal text-[#808080] leading-[1.5] max-w-prose text-pretty text-[18px]";
@@ -21,28 +20,9 @@ export default function Events() {
             opportunity for you to connect with great minds, get inspired, build
             a network and polish your ideas
           </p>
-          <p className={`${paragraphStyleClasses} `}>
-            Some of these events are:
-          </p>
         </div>
       </article>
-      <section className="flex flex-col gap-12 ">
-        {events.map((eachEvent, index) => (
-          <EventCard
-            key={index}
-            imageSrc={eachEvent.imageSrc}
-            end_time={eachEvent.end_time}
-            start_time={eachEvent.start_time}
-            eventDate={eachEvent.eventDate}
-            eventName={eachEvent.eventName}
-            description={eachEvent.description}
-            location={eachEvent.location}
-            calendarEndTime={eachEvent.calendarEndTime}
-            calendarStartDate={eachEvent.calendarStartDate}
-            calendarStartTime={eachEvent.calendarStartTime}
-          />
-        ))}
-      </section>
+      <EventCardsBlock />
     </section>
   );
 }
