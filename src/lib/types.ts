a013ceb4f44b88255512fa438e0e1a189cac3c24
Tom Okafor <tmeka008@gmail.com>
2025-11-19 13:03:16 +0100

    feat: add types for event api response and data object in response

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -30,8 +30,38 @@ export interface TeamAPIResponse {
   image_url: string;
 }
 
+enum EventType {
+  virtual = "VIRTUAL",
+  hybrid = "HYBRID",
+  physical = "PHYSICAL",
+}
+
+export interface EventsAPIResponseData {
+  id: string;
+  is_active: boolean;
+  created_at: string;
+  updated_at: string;
+  title: string;
+  event_type: EventType;
+  event_status: string;
+  image_url: string;
+  event_date: string;
+  start_time: string;
+  end_time: string;
+  event_link: string;
+  event_address: string;
+  register_link: string;
+}
+
+export interface EventsAPIResponse {
+  total: number;
+  page: number;
+  limit: number;
+  data: EventsAPIResponseData[];
+}
+
 export interface EventCardType {
-  imageSrc: StaticImageData;
+  imageSrc: StaticImageData | string;
   end_time: string;
   start_time: string;
   eventDate: string;
@@ -41,4 +71,6 @@ export interface EventCardType {
   calendarStartDate: string;
   calendarStartTime: string;
   calendarEndTime: string;
+  registrationLink: string;
+  eventType: EventType;
 }
