e407ebdbb8967ccdbe08b56dcb818cd599fc2f74
Tom Okafor <tmeka008@gmail.com>
2025-11-19 13:30:05 +0100

    feat: add new prop values and and add element to display event type

--- src/components/ui/EventCard.tsx
+++ src/components/ui/EventCard.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import ribbon from "../../../public/images/badge-ribbon.svg";
-import event_image from "../../../public/images/epicenter.webp";
 import Image from "next/image";
 import { CalendarDays, Clock8, MapPin } from "lucide-react";
 import Button from "../ui/Button";
@@ -22,11 +21,13 @@ export default function EventCard({
   calendarEndTime,
   calendarStartDate,
   calendarStartTime,
+  registrationLink,
+  eventType
 }: EventCardType) {
   return (
     <article className="flex items-stretch  gap-10 w-full pb-12 border-b-2 border-[#f1f1f1] group event-article">
-      <div className="relative w-[25%] aspect-square">
-        <div className="w-full h-full bg-aloe event-card relative rounded-bl-2xl overflow-hidden">
+      <div className="relative w-[25%] aspect-square event-parent">
+        <div className="w-full h-full bg-aloe event-card relative rounded-bl-2xl overflow-hidden border-2 border-black child">
           <Image
             src={imageSrc}
             alt="Event Image"
@@ -41,18 +42,25 @@ export default function EventCard({
         />
       </div>
       <article className="grow bg-[#f1f1f1] group-hover:bg-[#e0e0e0] duration-300 rounded-2xl p-4 px-8 flex flex-col justify-between gap-4 items-start">
-        <div className="flex gap-10 items-center p-2.5 px-4 bg-white rounded-full border border-aloe">
-          <div className="flex gap-1 items-center">
-            <CalendarDays className="text-aloe" />
-            <p className={paragraphStyleClasses}>{eventDate}</p>
+        <div className="flex items-center gap-6">
+          <div className="flex gap-10 items-center p-2.5 px-4 bg-white rounded-full border border-aloe h-full">
+            <div className="flex gap-1 items-center">
+              <CalendarDays className="text-aloe" />
+              <p className={paragraphStyleClasses}>{eventDate}</p>
+            </div>
+
+            <div className="h-full w-[2px] rounded-full bg-[#c0c0c0]"></div>
+            <div className="flex gap-1 items-center">
+              <Clock8 className="text-aloe" />
+              <p className={paragraphStyleClasses}>
+                {`${start_time} - ${end_time}`}
+              </p>
+            </div>
           </div>
-          <div className="h-full w-[2px] rounded-full bg-[#c0c0c0]"></div>
-          <div className="flex gap-1 items-center">
-            <Clock8 className="text-aloe" />
-            <p className={paragraphStyleClasses}>
-              {`${start_time} - ${end_time}`}
-            </p>
-          </div>
+
+          <p className="py-2 px-6 bg-[#ec208855] text-pink font-semibold tracking-[1.2px] font-body text-caption rounded-full shadow-lg">
+            {eventType}
+          </p>
         </div>
         <div className="flex flex-col gap-1.5">
           <h3 className="capitalize text-black font-body text-2xl font-semibold max-w-prose text-balance">
@@ -64,26 +72,38 @@ export default function EventCard({
           <MapPin className="text-aloe mr-2" />
           <p className={paragraphStyleClasses}>{location}</p>
         </div>
-        <AddToCalendarButton
-          name={eventName}
-          options={[
-            "Apple",
-            "Google",
-            "iCal",
-            "Microsoft365",
-            "MicrosoftTeams",
-            "Outlook.com",
-            "Yahoo",
-          ]}
-          location={location}
-          startDate={calendarStartDate}
-          startTime={calendarStartTime}
-          endTime={calendarEndTime}
-          timeZone="Africa/Lagos"
-          buttonStyle="round"
-          trigger="click"
-          description={description}
-        ></AddToCalendarButton>
+        <div className="flex gap-2 items-center">
+          <AddToCalendarButton
+            name={eventName}
+            options={[
+              "Apple",
+              "Google",
+              "iCal",
+              "Microsoft365",
+              "MicrosoftTeams",
+              "Outlook.com",
+              "Yahoo",
+            ]}
+            location={location}
+            startDate={calendarStartDate}
+            startTime={calendarStartTime}
+            endTime={calendarEndTime}
+            timeZone="Africa/Lagos"
+            buttonStyle="round"
+            trigger="click"
+            description={description}
+            inline={true}
+          ></AddToCalendarButton>
+          {registrationLink && (
+            <Button
+              classnames="reversed hov-aloe pr-8 tracking-[1px] pl-4"
+              text="register to attend"
+              onclick={() => {
+                window.open(registrationLink, "_blank");
+              }}
+            />
+          )}
+        </div>
       </article>
     </article>
   );
