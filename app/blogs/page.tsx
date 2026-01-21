fc2e2537e121cce3d190de31ba6e80f6c0251609
Tom Okafor <tmeka008@gmail.com>
2026-01-21 13:34:00 +0100

    refactor: remake blogs page and create new blog card and skeleton sub component

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -3,46 +3,118 @@ import { useState, useEffect } from "react";
 import Image from "next/image";
 import { fetchBlogs } from "@/src/lib/services";
 import { BlogAPIResponseData } from "@/src/lib/types";
-import BlogCard from "@/src/components/ui/BlogCard";
+import blog_page_image from "@/public/images/Cozy Workspace Setup.png";
+import { BlogCardType } from "@/src/lib/constants";
+import Button from "@/src/components/ui/Button";
+import { useRouter } from "next/navigation";
 
 export default function Team() {
   const [blogData, setBlogData] = useState<BlogAPIResponseData[] | null>(null);
-    useEffect(() => {
-      async function getBlogPosts() {
-        const blogPostsData = await fetchBlogs();
-        if (blogPostsData) setBlogData(() => blogPostsData.data);
-      }
-      getBlogPosts();
-    }, []);
+  useEffect(() => {
+    async function getBlogPosts() {
+      const blogPostsData = await fetchBlogs();
+      if (blogPostsData) setBlogData(() => blogPostsData.data);
+    }
+    getBlogPosts();
+  }, []);
 
   return (
-    <main className="px-8 py-20 flex flex-col gap-20 items-center">
-      <div>
-        <h1 className="font-figtree font-semibold text-aloe text-sub-heading leading-[1.3] max-w-[30ch]">
-          Insights, Stories, and Innovation!
-        </h1>
-        <p className="mt-5 font-body font-normal text-[#808080] text-body leading-[1.5] max-w-prose text-balance ">
-          Dive into the journeys of our finalists, learn best practices for
-          social entrepreneurship in Nigeria, and get expert advice on
-          developing your for-profit, for-people, and for-planet venture.
+    <main className="px-12">
+      <section className="flex flex-col items-center mb-20 mt-8">
+        <h2 className="font-figtree font-bold tracking-[-0.02em] text-hero text-black leading-[1.2]">
+          The Hult Pulse
+        </h2>
+        <p className="text-body max-w-prose text-center text-[#606060]">
+          Your central hub for official announcements, event highlights, and the
+          latest news from the Hult Prize Nigeria ecosystem.
         </p>
-      </div>
-
-      <section className="flex flex-col gap-30 items-center">
-        <div className="flex flex-col gap-10 items-center">
-          <div className="flex flex-wrap items-stretch gap-12 justify-center">
-            {blogData?.map((each_blog_data) => (
-                <BlogCard
-                id={each_blog_data.id}
-                key={each_blog_data.id}
-                imageSrc={each_blog_data.image_url}
-                title={each_blog_data.title.replaceAll("\\", "")}
-                author={each_blog_data.authour}
-                />
-            ))}
-          </div>
+      </section>
+      <section className="flex w-full gap-10 bg-[#cde8df] rounded-2xl mb-24 flex-col md:flex-row">
+        <article className="flex-1 flex flex-col pl-8 py-12">
+          <h3 className="font-bold text-4xl leading-[1.2] font-figtree text-aloe">
+            Documenting the Path Toward Sustainable Change
+          </h3>
+          <p className="mt-3 leading-[1.5] text-[#606060]">
+            Dive into the journeys of our finalists, learn best practices for
+            social entrepreneurship in Nigeria, and get expert advice on
+            developing your for-profit, for-people, and for-planet venture.
+          </p>
+          <p className="mt-auto text-[#606060]">{new Date().toDateString()}</p>
+        </article>
+        <div className="md:flex-[1.2] h-100 relative">
+          <Image
+            src={blog_page_image}
+            alt="Cozy Workspace Setup"
+            placeholder="blur"
+            fill
+            className="object-cover rounded-2xl"
+          />
         </div>
+      </section>
+
+      <section className="mb-20">
+        <h3 className="text-aloe font-semibold text-body-lg mb-8">
+          Blog Posts for you:
+        </h3>
+        <section className="flex flex-wrap gap-10">
+          {blogData
+            ? blogData.map((data, index) => (
+                <BlogCard
+                  key={index}
+                  id="the id"
+                  imageSrc="sdd"
+                  title="The blog post"
+                  author="Mr. Tom Jones"
+                />
+              ))
+            : Array.from("abacus").map((item, index) => (
+                <BlogCardSkeleton key={index} />
+              ))}
+        </section>
       </section>
     </main>
   );
 }
+
+const BlogCard = ({ id, imageSrc, title, author }: BlogCardType) => {
+  const router = useRouter();
+  return (
+    <article className="flex flex-col gap-6 w-[min(85vw,300px)] shrink-0 rounded-3xl bg-text-white pb-10 shadow-md">
+      <div className="bg-aloe relative group overflow-clip w-full h-[250px] rounded-2xl shadow-2xl">
+        <Image
+          src={blog_page_image}
+          alt="group photograh"
+          fill
+          className="object-cover group-hover:scale-120 duration-300 ease-out"
+        />
+      </div>
+      <div className="flex flex-col gap-0 pl-4">
+        <h3 className="font-bold text-black font-figtree text-body-lg capitalize leading-[1.3]">
+          {title}
+        </h3>
+        <p className="font-body text-[#707070] text-body font-medium mb-6">
+          {author}
+        </p>
+
+        <Button
+          text="read blog"
+          classnames="reversed button hov-pink bg-aloe grow-0 w-fit pr-6 pl-4 text-caption text-white font-normal hover:bg-black py-1"
+          onclick={() => {
+            router.push(`/blogs/${id}`);
+          }}
+        />
+      </div>
+    </article>
+  );
+};
+
+const BlogCardSkeleton = () => (
+  <div className="flex flex-col gap-6 w-[min(85vw,300px)] shrink-0 rounded-3xl bg-text-white pb-10 shadow-md">
+    <div className="bg-[#94e8cc] relative group overflow-clip w-full h-[250px] rounded-2xl shadow-2xl animate-pulse"></div>
+    <div className="flex flex-col gap-2 pl-4 *:animate-pulse">
+      <div className="w-[80%] h-4 bg-[#ababab88] rounded-sm"></div>
+      <div className="w-[60%] h-4 bg-[#ababab88] rounded-sm mb-6"></div>
+      <div className="w-[35%] h-6 bg-[#ababab88] rounded-lg"></div>
+    </div>
+  </div>
+);
