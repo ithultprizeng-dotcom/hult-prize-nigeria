6353a0c47894a0bad4d17fe5a7ee7307dcbb28b3
Tom Okafor <tmeka008@gmail.com>
2026-01-21 21:56:39 +0100

    refactor: format texts to replace escape characters

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Image from "next/image";
 import { fetchBlogs } from "@/src/lib/services";
 import { BlogAPIResponseData } from "@/src/lib/types";
@@ -7,8 +7,50 @@ import blog_page_image from "@/public/images/Cozy Workspace Setup.png";
 import { BlogCardType } from "@/src/lib/constants";
 import Button from "@/src/components/ui/Button";
 import { useRouter } from "next/navigation";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 export default function Team() {
+  const container = useRef(null);
+  useGSAP(
+    () => {
+      const headingSplit = new SplitText(".blog-heading", {
+        type: "chars, words, lines",
+        mask: "chars",
+      });
+      const subHeadingSplit = new SplitText(".blog-subheading", {
+        type: "lines",
+        mask: "lines",
+      });
+      gsap.set(headingSplit.chars, { yPercent: 110 });
+      gsap.set(subHeadingSplit.lines, { yPercent: 110 });
+      gsap.set([".blog-heading", ".blog-subheading"], { autoAlpha: 1 });
+      gsap.to(headingSplit.chars, {
+        yPercent: 0,
+        ease: "power4.out",
+        stagger: 0.05,
+        delay: 0.2,
+      });
+      gsap.to(subHeadingSplit.lines, {
+        yPercent: 0,
+        ease: "power4.out",
+        stagger: 0.1,
+        delay: 0.4,
+      });
+      gsap.to(".blog-banner", {
+        y: 0,
+        opacity: 1,
+        scale: 1,
+        ease: "power4.out",
+        duration: 0.8,
+        delay: 0.8,
+      });
+    },
+    { scope: container },
+  );
   const [blogData, setBlogData] = useState<BlogAPIResponseData[] | null>(null);
   useEffect(() => {
     async function getBlogPosts() {
@@ -19,18 +61,18 @@ export default function Team() {
   }, []);
 
   return (
-    <main className="px-8 md:px-12">
+    <main ref={container} className="px-8 md:px-12">
       <section className="flex flex-col items-center mb-20 mt-8">
-        <h2 className="font-figtree font-bold tracking-[-0.02em] text-hero text-black leading-[1.2]">
+        <h2 className="font-figtree font-bold tracking-[-0.02em] text-hero text-black leading-[1.2] blog-heading text-center invisible">
           The Hult Pulse
         </h2>
-        <p className="text-body max-w-prose text-center text-[#606060]">
+        <p className="text-body max-w-prose text-center text-[#606060] invisible blog-subheading">
           Your central hub for official announcements, event highlights, and the
           latest news from the Hult Prize Nigeria ecosystem.
         </p>
       </section>
-      <section className="flex w-full gap-10 bg-[#cde8df] rounded-2xl mb-24 flex-col md:flex-row">
-        <article className="flex-1 flex flex-col px-6 md:pl-8 md:pr-0 py-12 text-pretty">
+      <section className="flex w-full gap-10 bg-[#e6f4ef] rounded-2xl mb-24 flex-col md:flex-row opacity-0 blog-banner translate-y-20 scale-75">
+        <article className="flex-1 flex flex-col px-6 md:pl-10 md:pr-0 py-12 text-pretty">
           <h3 className="font-bold text-4xl leading-[1.2] font-figtree text-aloe text-pretty">
             Documenting the Path Toward Sustainable Change
           </h3>
@@ -39,7 +81,7 @@ export default function Team() {
             social entrepreneurship in Nigeria, and get expert advice on
             developing your for-profit, for-people, and for-planet venture.
           </p>
-          <p className="mt-auto text-[#606060]">{new Date().toDateString()}</p>
+          <p className="mt-auto text-aloe font-semibold">Hult Prize Nigeria - {new Date().toDateString()}</p>
         </article>
         <div className="md:flex-[1.2] h-100 relative">
           <Image
@@ -90,10 +132,10 @@ const BlogCard = ({ id, imageSrc, title, author }: BlogCardType) => {
       </div>
       <div className="flex flex-col gap-0 pl-4 pr-2">
         <h3 className="font-bold text-black font-figtree text-body-lg capitalize leading-[1.3] mb-2">
-          {title}
+          {title.replaceAll("\\", "")}
         </h3>
-        <p className="font-body text-[#707070] text-body font-medium mb-6">
-          {author}
+        <p className="font-body text-[#707070] text-body font-medium mb-6 capitalize">
+          {author.toLocaleLowerCase()}
         </p>
 
         <Button
