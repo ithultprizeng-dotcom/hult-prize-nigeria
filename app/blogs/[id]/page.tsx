09452552c6dba932aa55c3d6c18620a4d0f16e44
Tom Okafor <tmeka008@gmail.com>
2026-01-28 07:38:50 +0100

    feat: create generateMetaData function to create specific metadata for individual blog post

--- app/blogs/[id]/page.tsx
+++ app/blogs/[id]/page.tsx
@@ -1,67 +1,39 @@
-"use client";
-import { useParams } from "next/navigation";
-import Image from "next/image";
-import { notFound } from "next/navigation";
-import { useEffect, useRef, useState } from "react";
-import { BlogAPIResponseData } from "@/src/lib/types";
 import { fetchSingleBlogPost } from "@/src/lib/services";
+import BlogContent from "./BlogContent"; // Your client code below
+import { Metadata } from "next";
 
-export default function Blog() {
-  const { id } = useParams();
-  const sectionRef = useRef<HTMLElement>(null);
-  const [blogPost, setBlogPost] = useState<BlogAPIResponseData | null>(null);
-  const [html, sethtml] = useState("");
-  useEffect(() => {
-    async function getSingleBlogPost() {
-      if (!id) return;
-      const singleBlogPost = await fetchSingleBlogPost(id?.toString());
-      setBlogPost(singleBlogPost);
-      const blogContent = singleBlogPost.content;
+type Props = {
+  params: Promise<{ id: string }>;
+};
 
-      const cleanedBlogContent = blogContent
-        .replace(/\\r\\n/g, "")
-        .replace(/&lt;/g, "<")
-        .replace(/&gt;/g, ">")
-        .replace(/&amp;/g, "&")
-        .replace(/&#39;/g, "'")
-        .replace(/&quot;/g, '"')
-        .replaceAll("\\", "");
-      sethtml(cleanedBlogContent);
-    }
-    getSingleBlogPost();
-  }, []);
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { id } = await params;
+  const post = await fetchSingleBlogPost(id);
 
-  return blogPost ? (
-    <main className="flex flex-col gap-10 px-12 py-20">
-      <h2 className="text-main-heading text-aloe font-figtree font-bold capitalize max-w-[30ch] text-balance leading-[1.4]">
-        {blogPost?.title.toLowerCase().replaceAll("\\", "")}
-      </h2>
-      <Image
-        src={blogPost ? blogPost.image_url : ""}
-        alt={blogPost ? blogPost.title : ""}
-        width={1440}
-        height={800}
-        className="w-full object-contain rounded-2xl"
-      />
-      <section
-        ref={sectionRef}
-        className="blog-content flex flex-col gap-4"
-        dangerouslySetInnerHTML={{ __html: html }}
-      ></section>
-    </main>
-  ) : (
-    <Skeleton />
-    // <div className="min-h-dvh text-black text-body-lg font-body font-bold py-20 px-8">
-    //   Fetching blog. Please wait...
-    // </div>
-  );
+  if (!post) return { title: "Post Not Found" };
+
+  return {
+    title: post.title.replaceAll("\\", ""),
+    description: "Read more about " + post.title,
+    openGraph: {
+      title: post.title,
+      images: [post.image_url],
+    },
+    twitter: {
+    card: "summary_large_image",
+    site: "@HultPrizeNG",
+    creator: "@HultPrizeNG",
+      title: post.title,
+      images: [post.image_url],
+      description: "Read more about " + post.title,
+
+    },
+  };
 }
 
-function Skeleton() {
-  return <section className="flex flex-col gap-6 items-center">
-    <div>
-      <div> </div>
-      <div></div>
-    </div>
-  </section>;
+export default async function Page({ params }: Props) {
+  const { id } = await params;
+  const post = await fetchSingleBlogPost(id);
+
+  return  <BlogContent initialPost={post} />;
 }
