8e14556434c80259031d1b55e22d259c5f377bd8
Tom Okafor <tmeka008@gmail.com>
2026-03-25 22:11:24 +0100

    feat: create the competition page

---
+++ app/the-competition/page.tsx
@@ -0,0 +1,9 @@
+import { TheCompetitionHero } from "./components/Hero";
+
+export default function TheCompetition() {
+  return (
+    <main className="mt-16">
+      <TheCompetitionHero />
+    </main>
+  );
+}
