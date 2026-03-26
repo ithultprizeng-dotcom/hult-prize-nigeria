8c1f03eb53fc70622b05bfc38c6b448c86a3a67a
Tom Okafor <tmeka008@gmail.com>
2026-03-26 19:58:49 +0100

    feat: add new sections to the competition page

--- app/the-competition/page.tsx
+++ app/the-competition/page.tsx
@@ -1,9 +1,17 @@
+import { AboutTheCompetition } from "./components/About";
+import { TheCompetitionDetails } from "./components/Details";
+import { Experience } from "./components/Experience";
+import { Featuring } from "./components/Featuring";
 import { TheCompetitionHero } from "./components/Hero";
 
 export default function TheCompetition() {
   return (
     <main className="mt-16">
       <TheCompetitionHero />
+      <TheCompetitionDetails />
+      <AboutTheCompetition />
+      <Featuring />
+      <Experience />
     </main>
   );
 }
