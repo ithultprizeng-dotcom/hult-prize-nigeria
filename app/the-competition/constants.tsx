8c3f15b32b68202c660522db5d570da53e5be3c0
Tom Okafor <tmeka008@gmail.com>
2026-03-26 19:54:10 +0100

    feat: create constants for static data needed in the competition page

---
+++ app/the-competition/constants.tsx
@@ -0,0 +1,23 @@
+export interface FeaturingType {
+  id: string;
+  text: string;
+}
+
+export const featuringData: FeaturingType[] = [
+  { id: "01", text: "Dynamic, high-energy environment" },
+  { id: "02", text: "Real-world problem-solving challenges" },
+  { id: "03", text: "Engagement with industry leaders and decision-makers" },
+  { id: "04", text: "Amplification of visibility and credibility" },
+  {
+    id: "05",
+    text: "Access to a network of venture capitalists and investors",
+  },
+];
+
+export const participantData: FeaturingType[] = [
+  { id: "01", text: "Ambitious students and young individuals" },
+  { id: "02", text: "Innovators, social entrepreneurs, and creators" },
+  { id: "03", text: "Investors and venture capitalists" },
+  { id: "04", text: "Government and Development partners" },
+  { id: "05", text: "Ecosystem Partners" },
+];
