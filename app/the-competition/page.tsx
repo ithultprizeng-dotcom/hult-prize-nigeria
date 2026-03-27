d95f7c1a7afa2bced387c3b90eedc5f2ea837331
Tom Okafor <tmeka008@gmail.com>
2026-03-27 07:43:43 +0100

    feat: add all sub components to make up the competition page

--- app/the-competition/page.tsx
+++ app/the-competition/page.tsx
@@ -3,15 +3,17 @@ import { TheCompetitionDetails } from "./components/Details";
 import { Experience } from "./components/Experience";
 import { Featuring } from "./components/Featuring";
 import { TheCompetitionHero } from "./components/Hero";
+import { Locations } from "./components/Locations";
 
 export default function TheCompetition() {
   return (
-    <main className="mt-16">
+    <main className="mt-8 sm:mt-12 lg:mt-16">
       <TheCompetitionHero />
       <TheCompetitionDetails />
       <AboutTheCompetition />
       <Featuring />
       <Experience />
+      <Locations />
     </main>
   );
 }
