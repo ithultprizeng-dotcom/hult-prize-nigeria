5441e941a81c4d53adb85e68a62f79b8f0e604e7
Tom Okafor <tmeka008@gmail.com>
2026-04-15 08:42:11 +0100

    fix: align partners in grid

--- src/components/layout/Partners.tsx
+++ src/components/layout/Partners.tsx
@@ -35,18 +35,18 @@ export default function Partners() {
       <p className="text-black text-center font-body">
         We are proud to collaborate with these incredible organizations.
       </p>
-      <div className="flex flex-wrap justify-center items-center gap-10 sm:gap-8 mt-16 sm:mt-25">
+      <div className="flex flex-wrap justify-center items-stretch gap-10 sm:gap-8 mt-16 sm:mt-25">
         {partnersData?.map((partner) => (
           <div
             key={partner.id}
-            className="bg-white p-4 rounded-lg shadow-md border-2 border-text-white"
+            className="bg-white p-4 rounded-lg shadow-md border-2 border-text-white w-[min(250px,85vw)]"
           >
             <Image
               src={partner.image_url}
               alt={partner.name}
               width={200}
               height={200}
-              className="w-[min(200px,85vw)] h-[min(200px,40vw)] object-contain"
+              className="w-[100%] h-[min(200px,40vw)] object-contain"
             />
             <p className="text-aloe max-w-[40ch] text-pretty text-center font-body mt-2 text-body">
               {partner.name}
