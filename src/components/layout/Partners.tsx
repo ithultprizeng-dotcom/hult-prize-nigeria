4cc0819faf4b5d224e5cd6ffb821182881871134
Tom Okafor <tmeka008@gmail.com>
2026-04-15 08:27:55 +0100

    feat: restyle label paragraph for partners item

--- src/components/layout/Partners.tsx
+++ src/components/layout/Partners.tsx
@@ -37,15 +37,20 @@ export default function Partners() {
       </p>
       <div className="flex flex-wrap justify-center items-center gap-10 sm:gap-8 mt-16 sm:mt-25">
         {partnersData?.map((partner) => (
-          <div key={partner.id} className="bg-white p-4 rounded-lg shadow-md border-2 border-text-white">
+          <div
+            key={partner.id}
+            className="bg-white p-4 rounded-lg shadow-md border-2 border-text-white"
+          >
             <Image
               src={partner.image_url}
               alt={partner.name}
               width={200}
-              height={100}
+              height={200}
               className="w-[min(200px,85vw)] h-[min(200px,40vw)] object-contain"
             />
-            <p className="text-aloe text-center font-body mt-2 text-body">{partner.name}</p>
+            <p className="text-aloe max-w-[40ch] text-pretty text-center font-body mt-2 text-body">
+              {partner.name}
+            </p>
           </div>
         ))}
       </div>
