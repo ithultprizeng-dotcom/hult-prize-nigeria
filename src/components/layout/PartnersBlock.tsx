8d03f426fa5febf0bd43c8a1cd733f3322ea8f6a
Tom Okafor <tmeka008@gmail.com>
2026-05-04 11:07:07 +0100

    feat: create partners block component for reusable layout format for partners and sponsors sections

---
+++ src/components/layout/PartnersBlock.tsx
@@ -0,0 +1,42 @@
+import { PartnersAPIResponseData } from "@/src/lib/types";
+import Image from "next/image";
+
+export function PartnersBlock({
+  partnersData,
+  type,
+  text,
+  qualifier
+}: {
+  partnersData: PartnersAPIResponseData[] | null;
+  type: string;
+  text: string;
+  qualifier?: string;
+}) {
+  return (
+    <section className="min-h-screen bg-[url('/images/paper_background.png')] bg-cover bg-bottom py-20 pb-30 px-4 sm:px-8">
+      <h1 className="text-aloe text-main-heading font-figtree text-center font-bold">
+        Our <span className="text-pink">{qualifier || 'Amazing'}</span> {type}
+      </h1>
+      <p className="text-black text-center font-body">{text}</p>
+      <div className="flex flex-wrap justify-center items-stretch gap-10 sm:gap-8 mt-16 sm:mt-25">
+        {partnersData?.map((partner) => (
+          <div
+            key={partner.id}
+            className="bg-white p-4 rounded-lg shadow-md border-2 border-text-white w-[min(250px,85vw)]"
+          >
+            <Image
+              src={partner.image_url}
+              alt={partner.name}
+              width={200}
+              height={200}
+              className="w-[100%] h-[min(200px,40vw)] object-contain"
+            />
+            <p className="text-aloe max-w-[40ch] text-pretty text-center font-body mt-2 text-body">
+              {partner.name}
+            </p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
